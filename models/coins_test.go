1deb9ada376f5d44fef18d4742ff2dc87e7ab9a7
Cronos <eabz@polispay.org>
2019-09-11 06:54:15 -0500

    Fix

--- models/coins_test.go
+++ models/coins_test.go
@@ -11,6 +11,28 @@ func init() {
 	_ = godotenv.Load("../.env")
 }
 
+var TestCoinData = []Coin{
+	{"BTC", false, false, false, false, Balances{HotWallet: 1, Exchanges: 1,}},
+	{"LTC", false, false, false, false, Balances{HotWallet: 1, Exchanges: 1,}},
+	{"DASH", false, false, false, false, Balances{HotWallet: 1, Exchanges: 1,}},
+	{"POLIS", false, false, false, false, Balances{HotWallet: 1, Exchanges: 1,}},
+	{"GRS", false, false, false, false, Balances{HotWallet: 1, Exchanges: 1,}},
+	{"DGB", false, false, false, false, Balances{HotWallet: 1, Exchanges: 1,}},
+	{"COLX", false, false, false, false, Balances{HotWallet: 1, Exchanges: 1,}},
+}
+
+func TestCoinsModel_UpdateCoinsData(t *testing.T) {
+	db, err := config.ConnectDB()
+	assert.Nil(t, err)
+	model := CoinsModel{
+		Db:         db,
+		Collection: "coins",
+	}
+	assert.Nil(t, err)
+	err = model.UpdateCoinsData(TestCoinData)
+	assert.Nil(t, err)
+}
+
 func TestCoinsModel_GetCoinsData(t *testing.T) {
 	db, err := config.ConnectDB()
 	assert.Nil(t, err)
@@ -22,17 +44,6 @@ func TestCoinsModel_GetCoinsData(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotZero(t, len(coinsData))
 	assert.IsType(t, []Coin{}, coinsData)
+	assert.Equal(t, TestCoinData, coinsData)
 }
 
-func TestCoinsModel_UpdateCoinsData(t *testing.T) {
-	db, err := config.ConnectDB()
-	assert.Nil(t, err)
-	model := CoinsModel{
-		Db:         db,
-		Collection: "coins",
-	}
-	coinsData, err := model.GetCoinsData()
-	assert.Nil(t, err)
-	err = model.UpdateCoinsData(coinsData)
-	assert.Nil(t, err)
-}
