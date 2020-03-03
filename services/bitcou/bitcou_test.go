43b86a8b8d902adc059d8e742d988fe8aef11a79
helios <helios@polispay.org>
2020-03-03 00:09:19 -0600

    Removed Obsolete Tests

--- services/bitcou/bitcou_test.go
+++ services/bitcou/bitcou_test.go
@@ -2,23 +2,8 @@ package bitcou
 
 import (
 	"github.com/joho/godotenv"
-	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func init() {
 	_ = godotenv.Load()
-}
-
-func TestProviders(t *testing.T) {
-	bitcouService := InitService()
-	prodProv, err := bitcouService.GetProviders(false)
-	assert.Nil(t, err)
-	assert.NotNil(t, prodProv)
-
-	devProv, err := bitcouService.GetProviders(true)
-	assert.Nil(t, err)
-	assert.NotNil(t, devProv)
-
-	assert.NotEqual(t, devProv, prodProv)
 }
