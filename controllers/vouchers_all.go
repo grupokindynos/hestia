2b945eb58f03b6df4159c929b1ac8cbea56d6953
oedipusK <58442178+oedipusK@users.noreply.github.com>
2020-05-27 16:42:43 -0500

    Added shippingMethod and variantId for lightVoucher

--- controllers/vouchers_all.go
+++ controllers/vouchers_all.go
@@ -6,6 +6,7 @@ import (
 	"github.com/grupokindynos/common/hestia"
 	"github.com/grupokindynos/hestia/models"
 	"github.com/shopspring/decimal"
+	"strconv"
 )
 
 type VouchersAllController struct {
@@ -17,6 +18,7 @@ type VouchersAllController struct {
 func voucherToLightVoucher(voucher hestia.Voucher) hestia.LightVoucher {
 	dec := decimal.NewFromInt(voucher.PaymentData.Amount).Div(decimal.NewFromInt(1e8))
 	amount, _ := dec.Float64()
+	variantId, _ := strconv.Atoi(voucher.VariantID)
 	return hestia.LightVoucher{
 		Id:          voucher.ID,
 		VoucherId:   voucher.VoucherID,
@@ -28,6 +30,8 @@ func voucherToLightVoucher(voucher hestia.Voucher) hestia.LightVoucher {
 		RefundTxId:  "", // VouchersV1 don't have refund info.
 		Status: voucher.Status,
 		ProviderId: fmt.Sprintf("%d", voucher.ProviderId),
+		ShippingMethod: hestia.VoucherShippingMethodApi, // vouchersv1 don't have other option.
+		VariantId: variantId,
 	}
 }
 
@@ -45,6 +49,8 @@ func voucherV2toLightVoucher(voucher hestia.VoucherV2) hestia.LightVoucher {
 		RefundTxId:  voucher.RefundTxId,
 		Status: hestia.GetVoucherStatusV2String(voucher.Status),
 		ProviderId: voucher.ProviderId,
+		ShippingMethod: voucher.ShippingMethod,
+		VariantId: voucher.VariantId,
 	}
 }
 
--- go.mod
+++ go.mod
@@ -10,7 +10,7 @@ require (
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/golang/protobuf v1.3.3 // indirect
-	github.com/grupokindynos/common v0.0.0-20200527151404-b9fc79246204
+	github.com/grupokindynos/common v0.0.0-20200527213722-1e447dc94c37
 	github.com/joho/godotenv v1.3.0
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
