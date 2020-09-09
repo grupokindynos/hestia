b59430f859a6ed9d499794e51d323bfbeca537f0
helios <helios@polispay.org>
2020-09-09 13:52:34 -0500

    Update bitcou2.go

--- processor/bitcou2/bitcou2.go
+++ processor/bitcou2/bitcou2.go
@@ -59,7 +59,7 @@ func main() {
 
 
 	for _, bitcouCountry := range countries {
-		if bitcouCountry.ID == "CL" || bitcouCountry.ID == "DO" || bitcouCountry.ID == "MA" || bitcouCountry.ID == "KW" || bitcouCountry.ID == "UA" || bitcouCountry.ID == "VE"{
+		if bitcouCountry.ID == "CL" || bitcouCountry.ID == "DO" || bitcouCountry.ID == "MA" || bitcouCountry.ID == "KW" || bitcouCountry.ID == "VE"{
 			log.Println("Ignoring country ", bitcouCountry.ID)
 		} else {
 			err = model.AddCountryV2(bitcouCountry)
